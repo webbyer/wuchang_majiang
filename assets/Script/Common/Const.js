a708c9810d144408f407b2987463e54ffd66dabf
ycjia <webbyer@163.com>
2017-12-01 22:55:11 +0800

    更换为方言音效

--- assets/Script/Common/Const.js
+++ assets/Script/Common/Const.js
@@ -2,10 +2,10 @@
  * Created by pc on 2017/10/8.
  */
 module.exports = {
-    HOST_STR: "ws://cymj.91snack.com/websocket",
-    IMAGE_PREFIX_HOST: "http://cymj.91snack.com",
-    //HOST_STR: "ws://127.0.0.1/websocket",
-    //IMAGE_PREFIX_HOST: "http://127.0.0.1",
+    HOST_STR: "ws://wuchangmj.91snack.com:9090/websocket",
+    IMAGE_PREFIX_HOST: "http://wuchangmj.91snack.com:9090",
+    //HOST_STR: "ws://127.0.0.1:9090/websocket",
+    //IMAGE_PREFIX_HOST: "http://127.0.0.1:9090",
     IOS_DOWNLOAD_LINK: null,
     ANDROID_DOWNLOAD_LINK: null,
 };
